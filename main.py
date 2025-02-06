f4cf3f907a578330d7fc0b6a4ba351b6d992a14e
Elea Hinn <chleo.hinn@efrei.net>
2025-02-06 11:18:02 +0100

    innit

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
+def is_fizz(n):
+    return n % 3 == 0 or '3' in str(n)
 
-def main():
-    print('Hello, World!')
-    return 0
+def is_buzz(n):
+    return n % 5 == 0 or '5' in str(n)
 
-if __name__ == '__main__':
-    main()
+def fizzbuzz():
+    for i in range(1, 101):
+        result = ""
+        if is_fizz(i):
+            result += "Fizz"
+        if is_buzz(i):
+            result += "Buzz"
+        
+        print(result if result else i)
 
+fizzbuzz()
